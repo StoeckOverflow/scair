94f126b0777513dc553f7b4753e94ecd0fedb4a4
StoeckOverflow <dominic-st@gmx.de>
2026-02-17 11:50:49 +0100

    added  NoMemoryEffect to unrealized_conversion_cast

--- core/src/main/scala-3/builtin/Builtin.scala
+++ core/src/main/scala-3/builtin/Builtin.scala
@@ -544,7 +544,9 @@ case class UnrealizedConversionCastOp(
 ) extends DerivedOperation[
       "builtin.unrealized_conversion_cast",
       UnrealizedConversionCastOp,
-    ] derives DerivedOperationCompanion
+    ]
+    with NoMemoryEffect
+    derives DerivedOperationCompanion
 
 val BuiltinDialect =
   summonDialect[EmptyTuple, (ModuleOp, UnrealizedConversionCastOp)]
