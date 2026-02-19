3f8c366ce5f4140df825e4084be0624d788006b2
StoeckOverflow <dominic-st@gmx.de>
2026-02-19 11:35:37 +0100

    condensed dtensor scala test suite

--- dialects/test/src/scala-3/TensorApiTest.scala
+++ dialects/test/src/scala-3/TensorApiTest.scala
@@ -5,6 +5,7 @@ import org.scalatest.matchers.should.Matchers.*
 import scair.dialects.builtin.*
 import scair.dialects.dTensor.*
 import scair.ir.*
+import scair.utils.OK
 
 final class TensorTypesSpec extends AnyFlatSpec:
 
@@ -35,3 +36,20 @@ final class TensorTypesSpec extends AnyFlatSpec:
     val tt = dTensorTensorType(Seq(ValueAttribute(n)), Float32Type())
     dTensorTypeUtil.asdTensor(tt) shouldBe tt
   }
+
+  "tensor.dim" should
+    "return the embedded dim SSA value for the selected axis" in {
+      val d0 = Value[Attribute](dTensorNatType())
+      val d1 = Value[Attribute](dTensorNatType())
+      val tensor = Value[dTensorTensorType](
+        dTensorTensorType(
+          Seq(ValueAttribute(d0), ValueAttribute(d1)),
+          Float32Type(),
+        )
+      )
+      val dim =
+        Dim(tensor, IntegerAttr(IntData(1), I32), Result(dTensorNatType()))
+
+      dim.verify()
+      dim.selectedDimValue shouldBe OK(d1)
+    }
