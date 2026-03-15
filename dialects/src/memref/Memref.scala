75db7842f8e537dfa6e009de0dfc5ff981423b31
StoeckOverflow <dominic-st@gmx.de>
2026-03-15 15:46:46 +0100

    add reinterpret_cast and descriptor-form boundary ops

--- dialects/src/memref/Memref.scala
+++ dialects/src/memref/Memref.scala
@@ -49,5 +49,55 @@ case class Store(
 ) extends DerivedOperation["memref.store", Store]
     derives DerivedOperationCompanion
 
+case class ReinterpretCast(
+    src: Operand[MemrefType],
+    offset: Operand[IndexType],
+    sizes: Seq[Operand[IndexType]],
+    strides: Seq[Operand[IndexType]],
+    res: Result[MemrefType],
+) extends DerivedOperation["memref.reinterpret_cast", ReinterpretCast]
+    derives DerivedOperationCompanion
+
+case class DescriptorAlloc(
+    dynamicSizes: Seq[Operand[IndexType]],
+    descriptor: Result[Attribute],
+    source_type: Attribute,
+) extends DerivedOperation["memref.descriptor_alloc", DescriptorAlloc]
+    derives DerivedOperationCompanion
+
+case class DescriptorReinterpret(
+    _operands: Seq[Operand[Attribute]],
+    descriptor: Result[Attribute],
+    source_type: Attribute,
+    target_type: Attribute,
+) extends DerivedOperation["memref.descriptor_reinterpret", DescriptorReinterpret]
+    derives DerivedOperationCompanion
+
+case class DescriptorLoad(
+    _operands: Seq[Operand[Attribute]],
+    result: Result[Attribute],
+    source_type: Attribute,
+) extends DerivedOperation["memref.descriptor_load", DescriptorLoad]
+    derives DerivedOperationCompanion
+
+case class DescriptorDealloc(
+    descriptor: Operand[Attribute]
+) extends DerivedOperation["memref.descriptor_dealloc", DescriptorDealloc]
+    derives DerivedOperationCompanion
+
 val MemrefDialect =
-  summonDialect[EmptyTuple, (Alloc, Dealloc, Load, Store, Dim)]
+  summonDialect[
+    EmptyTuple,
+    (
+        Alloc,
+        Dealloc,
+        Load,
+        Store,
+        Dim,
+        ReinterpretCast,
+        DescriptorAlloc,
+        DescriptorReinterpret,
+        DescriptorLoad,
+        DescriptorDealloc,
+    ),
+  ]
